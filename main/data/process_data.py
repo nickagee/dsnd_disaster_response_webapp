23dbaaf3491b1167c6717fd0b0d839542e64ddb5
Nicholas Jackson <njackson.gis@gmail.com>
2021-08-26 19:36:45 -0400

    cleaning documentation

--- main/data/process_data.py
+++ main/data/process_data.py
@@ -1,6 +1,5 @@
 import sys
 import pandas as pd
-import numpy as np
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
@@ -75,12 +74,7 @@ def main():
         print('Cleaned data saved to database!')
     
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
+        print('Please provide : python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db')
 
 
 if __name__ == '__main__':
--- main/models/train_classifier.py
+++ main/models/train_classifier.py
@@ -1,21 +1,20 @@
 import sys, re, warnings, nltk, pickle
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer,classification_report
+from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.pipeline import Pipeline
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
@@ -35,7 +34,7 @@ def load_data(database_filepath):
         Y -> label DataFrame
         category_names -> used for data visualization (app)
     """
-    engine = create_engine('sqlite:///'+database_filepath)
+    engine = create_engine(f'sqlite:///{database_filepath}')
     df = pd.read_sql_table('my_disaster_response_table',engine)
     X = df['message']
     Y = df.iloc[:,4:]
@@ -58,11 +57,7 @@ def tokenize(text):
         text = text.replace(url, 'urlplaceholder')
     
     tokens = nltk.word_tokenize(re.sub(r"[^a-zA-Z0-9]", " ", text.lower()))
-    
-    # Remove stopwords
     tokens = [t for t in tokens if t not in stopwords.words('english')]
-
-    # Lemmatize
     lemmatizer=WordNetLemmatizer()
     tokens = [lemmatizer.lemmatize(t) for t in tokens]
     
@@ -91,8 +86,6 @@ def build_model():
     model=cv_ada
     return model
 
-
-
 def evaluate_model(model, X_test, Y_test, category_names):
     """Evaluate model
     Input:
@@ -105,16 +98,14 @@ def evaluate_model(model, X_test, Y_test, category_names):
     
     print(classification_report(Y_test.iloc[:, 1:].values, np.array([x[1:] for x in Y_pred]), target_names = category_names))
 
-
 def save_model(model, model_filepath):
     with open(model_filepath, 'wb') as file:
         pickle.dump(model, file)
 
-
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
-        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        print('Loading...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
@@ -133,11 +124,7 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
-
+        print('Please provide: python train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 if __name__ == '__main__':
     main()
